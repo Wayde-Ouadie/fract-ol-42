398eae82e9b4487cc987c6335101d5db687738c5
Ouadie El Fengour <oel-feng@e1r4p14.1337.ma>
2024-03-07 19:05:04 +0100

    first commit

---
+++ fractol.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 18:56:12 by oel-feng          #+#    #+#             */
+/*   Updated: 2024/03/07 19:04:36 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+# include <mlx.h>
+
+typedef struct s_fractol
+{
+    void	*mlx;
+    void	*win;
+}	t_fractol;
+
+
+
+
+
+#endif
